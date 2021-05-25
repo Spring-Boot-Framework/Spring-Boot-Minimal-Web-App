123cd5715f8831c7ceac1393d3d28a8772a51713
Anantha Raju C <arcswdev@gmail.com>
2021-05-25 15:03:56 +0530

    UPDATED controller with PUT, POST, DELETE endpoints
    - UPDATED application.properties
    - UPDATED .java files with log statement's
    - ADDED actuator,lombok dependencies
    - ADDED banner.txt

--- src/main/java/io/github/anantharajuc/sbmwa/SpringBootMinimalWebAppApplication.java
+++ src/main/java/io/github/anantharajuc/sbmwa/SpringBootMinimalWebAppApplication.java
@@ -1,13 +1,26 @@
 package io.github.anantharajuc.sbmwa;
 
+import java.time.LocalDateTime;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.log4j.Log4j2;
+
+/*
+ * Spring Boot Minimal Web App 
+ *
+ * @author <a href="mailto:arcswdev@gmail.com">Anantha Raju C</a>
+ *
+ */
+@Log4j2
 @SpringBootApplication
 public class SpringBootMinimalWebAppApplication 
 {
 	public static void main(String[] args) 
 	{
 		SpringApplication.run(SpringBootMinimalWebAppApplication.class, args);
+		
+		log.info("-----> Started Spring Boot Minimal Web App - "+LocalDateTime.now());
 	}
 }
--- src/main/java/io/github/anantharajuc/sbmwa/controller/HelloWorldController.java
+++ src/main/java/io/github/anantharajuc/sbmwa/controller/HelloWorldController.java
@@ -2,15 +2,48 @@ package io.github.anantharajuc.sbmwa.controller;
 
 import java.time.LocalDateTime;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
 @RestController("/")
 public class HelloWorldController 
 {
 	@GetMapping()
-	public String helloWorld() 
+	public ResponseEntity<String> helloWorld() 
 	{
-		return "Greetings from Spring Boot Minimal Web Application! - "+LocalDateTime.now();
+		log.info("-----> HTTP GET Handled - "+LocalDateTime.now());
+		
+		return ResponseEntity.ok("HTTP GET Handled - "+LocalDateTime.now());
+	}
+	
+	@PostMapping
+	public ResponseEntity<String> createProduct()
+	{
+		log.info("-----> HTTP POST Handled - "+LocalDateTime.now());
+		
+		return ResponseEntity.ok("HTTP POST Handled - "+LocalDateTime.now());
+	}
+	
+	@PutMapping
+	public ResponseEntity<String> updateProduct()
+	{
+		log.info("-----> HTTP PUT Handled - "+LocalDateTime.now());
+		
+		return ResponseEntity.ok("HTTP PUT Handled - "+LocalDateTime.now());
+	}
+	
+	@DeleteMapping
+	public ResponseEntity<String> deleteProduct()
+	{
+		log.info("-----> HTTP DELETE Handled - "+LocalDateTime.now());
+		
+		return ResponseEntity.ok("HTTP DELETE Handled - "+LocalDateTime.now());
 	}
 }
